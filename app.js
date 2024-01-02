b08527680ca601a1dd82675ed100bf6dfcbcfbc6
Jahdai <jahdai.kilkenny@gmail.com>
2024-01-02 07:59:48 -0500

    Welcome Screen Modal created

---
+++ app.js
@@ -0,0 +1,6 @@
+const modalButton = document.getElementById("modalbutton");
+const welcomeModal = document.getElementById("welcomemodal");
+
+modalButton.addEventListener("click", () => {
+    welcomeModal.style.display = "none";
+  });
